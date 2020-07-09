19996ec0b32f80f3ff3a9da06d1aff94bc902f42
Edgaras <twixas99@gmail.com>
2020-07-10 02:15:10 +0300

    3 task completed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,12 +7,21 @@ class Lektuvas {
     public void iskeistiVaziuokle()
     {
         Random rand = new Random();
-        int skaicius = rand.nextInt(2);
+        int skaicius = rand.nextInt(6);
         try {
-            int bandom = 5 / skaicius;
-            System.out.println("OK: važiuoklė sėkmingai išskleista - nusileidom");
-            } catch (ArithmeticException e) {
-            System.out.println("ERROR : nepavyko isskleisti vaziuokles - suduzom");
+            int bandom = 4 / skaicius;
+            if (skaicius == 5)
+            {
+                "skrendu".charAt(20);
+            }
+            System.out.println("OK : važiuoklė sėkmingai išskleista");
+            } catch (ArithmeticException e) { // divided by 0
+            System.out.println("ERROR : nepavyko isskleisti vaziuokles");
+            } catch (StringIndexOutOfBoundsException e){
+            System.out.println("ERROR : nepavyko išskleisti važiuoklės dėl kitos klaidos");
+            } finally {
+            System.out.println("INFO: lektuvas skrenda");
+            System.out.println("\n");
         }
     }
 

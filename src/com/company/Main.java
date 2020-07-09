5f73ff8329e2436837687eeaa279f7306cfa17cf
Edgaras <twixas99@gmail.com>
2020-07-09 20:00:35 +0300

    Lektuvo vaziuokles kodo pradzia

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,32 @@
 package com.company;
 
+import java.util.Random;
+
+class Lektuvas {
+
+    public void iskeistiVaziuokle()
+    {
+        Random rand = new Random();
+        int skaicius = rand.nextInt(10);
+        int bandom = 5 / skaicius;
+        System.out.println("OK: važiuoklė sėkmingai išskleista");
+
+    }
+
+}
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Antra eilute, kad persikelt koda i intellij");        
+        System.out.println("Antra eilute, kad persikelt koda i intellij");
+
+        Lektuvas lekt = new Lektuvas();
+        lekt.iskeistiVaziuokle();
+        lekt.iskeistiVaziuokle();
+        lekt.iskeistiVaziuokle();
+        lekt.iskeistiVaziuokle();
+        lekt.iskeistiVaziuokle();
+
     }
 }

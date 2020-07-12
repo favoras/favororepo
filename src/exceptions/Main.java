dbe26b1b024fbf37eda56170edff1459d73baa6f
Edgaras <twixas99@gmail.com>
2020-07-12 21:17:04 +0300

    Exceptions task 2 completed

--- src/exceptions/Main.java
+++ src/exceptions/Main.java
@@ -1,37 +1,35 @@
 package exceptions;
 
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
-        A a = new A();
-
-        try {
-            a.a1();
-        } catch (StackOverflowError e) {
-            System.out.println("Stack Overflow Error");
+        Input input = new Input();
+        boolean argerai = false;
+        while(!argerai)
+        {
+            try {
+                input.Ivesk();
+                argerai = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Input Mismatch Exception");
+                System.out.println("Bandykite dar karta.");
+            }
         }
+
     }
 }
 
-class A {
-    public void a1() {
-        B b = new B();
-        b.metodas1();
+class Input {
+    public void Ivesk(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Iveskite sveikaji skaiciu");
+        int skaicius = sc.nextInt();
+        System.out.println("Jusu ivestas skaicius yra: " + skaicius);
+
+
     }
 
-}
-
-class B {
-    public void metodas1(){
-        C c = new C();
-        c.metodas2();
-    }
-
-}
-
-class C {
-    public void metodas2(){
-        metodas2();
-    }
 }

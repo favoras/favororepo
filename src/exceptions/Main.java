3a88b97e4b2e550ff43b6820a4ff9104bc41b2f7
Edgaras <twixas99@gmail.com>
2020-07-13 13:03:08 +0300

    3 task not completed

--- src/exceptions/Main.java
+++ src/exceptions/Main.java
@@ -1,35 +1,92 @@
 package exceptions;
 
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        Input input = new Input();
-        boolean argerai = false;
-        while(!argerai)
-        {
-            try {
-                input.Ivesk();
-                argerai = true;
-            } catch (InputMismatchException e) {
-                System.out.println("Input Mismatch Exception");
-                System.out.println("Bandykite dar karta.");
-            }
-        }
+    public static void main(String[] args) throws DeviceIsOffException {
+        
+        Device dev = new Device();
+        dev.ping();
+        
+     //     List<Device> devices = new ArrayList<>();
+     //     devices.add(new Mouse(true));
+     //     devices.add(new Display(true));
 
     }
 }
 
-class Input {
-    public void Ivesk(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Iveskite sveikaji skaiciu");
-        int skaicius = sc.nextInt();
-        System.out.println("Jusu ivestas skaicius yra: " + skaicius);
+class Device {
+    
 
+    public void ping() throws DeviceIsOffException {
+
+        Mouse mouse = new Mouse();
+        Display display = new Display();
+        Keyboard keyboard = new Keyboard();
+
+        mouse.mouseCheck();
+        display.displayCheck();
+        keyboard.keyboardCheck();
 
     }
+}
 
+class Mouse extends Device {
+
+    boolean isOn = true;
+
+    public void mouseCheck() throws DeviceIsOffException {
+       if (isOn = true){
+           System.out.println("Mouse working");
+       }
+       else {
+           throw new DeviceIsOffException("Mouse not working");
+       }
+   }
+
+}
+class Display extends Device {
+
+    boolean isOn = true;
+
+    public void displayCheck() throws DeviceIsOffException {
+        if (isOn = true){
+            System.out.println("Display working");
+        }
+        else {
+            throw new DeviceIsOffException("Display not working");
+        }
+    }
+
+}
+class Keyboard extends Device {
+
+    boolean isOn = true;
+
+    public void keyboardCheck() throws DeviceIsOffException {
+        if (isOn = true){
+            System.out.println("Keyboard working");
+        }
+        else {
+            throw new DeviceIsOffException("Keyboard not working");
+        }
+    }
+}
+
+
+class DeviceIsOffException extends Exception {
+
+    public DeviceIsOffException(String message) {
+        super(message);
+    }
+
+}
+
+class Monitoring {
+
+    public void pingAllDevices() {
+
+    }
 }

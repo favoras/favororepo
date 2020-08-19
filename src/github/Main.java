312af21f3083842e9098aaebc1b6c9dcae617c92
Edgaras Puzevičius <38849005+favoras@users.noreply.github.com>
2020-08-19 20:53:11 +0300

    Sprendimas idetas

--- src/github/Main.java
+++ src/github/Main.java
@@ -1,4 +1,14 @@
 package github;
 
-public class Main {
+public class Uzduotis1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String sakinys = sc.nextLine();
+
+		String[] z1 = sakinys.split(" ");
+
+		System.out.println("Pirmas sakinio žodis: " + z1[0] + ".");
+    }
 }
